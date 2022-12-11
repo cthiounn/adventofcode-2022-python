5877d6bad98c099afa73fa777c70442fad545824
cthiounn <thiounn.conrad@gmail.com>
2022-12-11 10:19:39 +0000

    d11 p1p2

--- day11.py
+++ day11.py
@@ -25,15 +25,10 @@ class Monkey():
         if self.items:
             item=self.items[0]
             self.items=self.items[1:]
-            new_item=self.function(item) % reduce(mul,[m.intnumdiv for _,m in self.monkeys.items()])
-
-            # print(item,self.function,new_item)
-            # print(self.testfunction(new_item))
+            new_item=self.function(item) //3
             if self.testfunction(new_item):
-                # print("True send to",self.num_true)
                 self.send_item(new_item,self.num_true,self.monkeys)
             else:
-                # print("False send to",self.num_false)
                 self.send_item(new_item,self.num_false,self.monkeys)
             self.count+=1
 
@@ -41,10 +36,23 @@ class Monkey():
         for _ in range(len(self.items)):
             self.inspect_item()               
 
+class Monkey2(Monkey):
+    def inspect_item(self):
+        if self.items:
+            item=self.items[0]
+            self.items=self.items[1:]
+            new_item=self.function(item) % reduce(mul,[m.intnumdiv for _,m in self.monkeys.items()])
+            if self.testfunction(new_item):
+                self.send_item(new_item,self.num_true,self.monkeys)
+            else:
+                self.send_item(new_item,self.num_false,self.monkeys)
+            self.count+=1
+
 
 def part1and2(lines):
     i=0
     dmonkeys=dict()
+    dmonkeys2=dict()
     items=[]
     function=lambda x:x
     testfunction=lambda x:x
@@ -64,6 +72,7 @@ def part1and2(lines):
             num_false=int(regnum(l)[0])      
         elif l=="":
             dmonkeys[i]=Monkey(i,items,function,testfunction,num_true,num_false,dmonkeys,int(numdiv))
+            dmonkeys2[i]=Monkey2(i,items.copy(),function,testfunction,num_true,num_false,dmonkeys2,int(numdiv))
             items=[]
             function=lambda x:x
             testfunction=lambda x:x
@@ -71,11 +80,12 @@ def part1and2(lines):
             num_false=0
             i+=1
     dmonkeys[i]=Monkey(i,items,function,testfunction,num_true,num_false,dmonkeys,int(numdiv))
-
+    dmonkeys2[i]=Monkey2(i,items.copy(),function,testfunction,num_true,num_false,dmonkeys2,int(numdiv))
     for j in range(1,10001):
         for i in range(len(dmonkeys)):
             dmonkeys[i].inspect_all_items()
-    for i in range(len(dmonkeys)):
-        print(dmonkeys[i].items)
-    print(reduce(mul,sorted([m.count for _,m in dmonkeys.items()])[-2:]))
-print(part1and2(lines))
+            dmonkeys2[i].inspect_all_items()
+        if j==20:
+            print(reduce(mul,sorted([m.count for _,m in dmonkeys.items()])[-2:]))
+    print(reduce(mul,sorted([m.count for _,m in dmonkeys2.items()])[-2:]))
+part1and2(lines)
